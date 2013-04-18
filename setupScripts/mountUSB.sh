8c584438fd54469de41d9453f4ed5203875df428
tsukolsky <tsukolsky@gmail.com>
2013-04-18 03:29:44 +0000

    Finished setupScripts, works. Could add 'dmesg -c' command in very beginning, also could tweak to recognize any sd*1, then assign that as the path, recognizes from sda->sdg

--- setupScripts/mountUSB.sh
+++ setupScripts/mountUSB.sh
@@ -12,43 +12,60 @@ clear
 
 #Initialize LASTOUTPUT
 LASTOUTPUT=''
+MOUNTDIR=/media/USB
+initial=`ls /media/USB`
+if [ -z "$initial" ]; then
+	LASTOUTPUT=`dmesg | tail -f | grep 'sda\|sdb\|sdc' | awk '{print $2}' | cut -d "]" -f 1`
+fi
 
-while true
-do
+##While true loop. Always do this
+while true; do
 	##Lower GPIO line
-	echo 0 > /sys/class/gpio/gpio/value
-	echo "GPIO line low"
+	echo 0 > /sys/class/gpio/gpio27/value
+#	echo "GPIO line low"
 
 	# Looking for USB drives using the mount cmd.
 	# Assuming the drive will be /dev/sda1 or /dev/sdb1 or /dev/sdc1
-	while :
-	do
-		echo "Checking for USB insert"
+	while true; do
 		sleep 5
-		command='dmesg | tail -f | grep 'sda\|sdb\|sdc' | awk '{print $2}' | cut -d "]" -f 1'
-		IOUTPUT=`$command`
-		if [[ -z "$IOUTPUT" ] && [ "$IOUTPUT" != "$LASTOUTPUT" ]]
-		then
-			echo "USB inserted"
+		echo "Checking for USB insert"
+		IOUTPUT=`dmesg | tail -f | grep 'sda\|sdb\|sdc\|sdd\|sde\|sdf\|sdg' | awk '{print $2}' | cut -d "]" -f 1`
+		#If there output of that string is null Or that is the same as last output, there isn't a new USB
+		if [ -n "$IOUTPUT" -a "$IOUTPUT" != "$LASTOUTPUT" ]; then
 			break
 		else
-			echo "No new USB"
-	done
+			echo "No difference"
+		fi
+	done #end blocking while  waiting for new usb
 		
 	LASTOUTPUT=$IOUTPUT
 
 	MOUNTDIR=/media/USB
 
 	#See which drive partition is was applied under, default is sda1
-	if dmesg | grep -q "sdb1"; then
+	if dmesg | tail -f | grep -q "sdb1"; then
 		USBNAME=/dev/sdb1
-	elif dmesg | grep -q "sdc1"; then
+		echo "I think sdb1"
+	elif dmesg | tail -f | grep -q "sdc1"; then
 		USBNAME=/dev/sdc1
+		echo "I think sdc1"
+	elif dmesg | tail -f | grep -q "sdd1"; then
+		USBNAME=/dev/sdd1
+		echo "I think sdd1"
+	elif dmesg | tail -f | grep -q "sde1"; then
+		USBNAME=/dev/sde1
+		echo "I think sde1"
+	elif dmesg | tail -f | grep -q "sdf1"; then
+		USBNAME=/dev/sdf1
+		echo "I think sdf1"
+	elif dmesg | tail -f | grep -q "sdg1"; then
+		USBNAME=/dev/sdg1
 	else
 		USBNAME=/dev/sda1
+		echo "I think sda1"
 	fi
 
-	echo "Mounting $USBNAME to $MOUNTDIR..."
+#	echo "Mounting $USBNAME to $MOUNTDIR..."
 	
 	# Creating the dir /media/USB (if it does not exist)
 	if [ ! -d $MOUNTDIR ]; then
@@ -56,34 +73,32 @@ do
 	fi
 	
 	# Mount the drive
-	mount -t vfat -o rw,users $USBNAME $MOUNTDIR
+	mount -t vfat $USBNAME $MOUNTDIR
 	
 	#Raise GPIO line to GAVR
-	echo 1 > /sys/class/gpio/gpio/value
-	echo "GPIO line high."
+	echo 1 > /sys/class/gpio/gpio27/value
+#	echo "GPIO line high."
 	
 	#Setup the files if they don't already exist
-	if [ ! -d $TEMPDIR/ReCycle ]; then
+	if [ ! -d $MOUNTDIR/ReCycle ]; then
 		mkdir $MOUNTDIR/ReCycle
 	fi
 	if [ ! -d $MOUNTDIR/ReCycle/Trips ]; then
-		mkdir $TEMPDIR/ReCycle/Trips
+		mkdir $MOUNTDIR/ReCycle/Trips
+		echo 0 > $MOUNTDIR/ReCycle/Trips/.info
 	fi
 	if [ ! -d $MOUNTDIR/ReCycle/gps ]; then
 		mkdir $MOUNTDIR/ReCycle/gps
+		echo 0 > $MOUNTDIR/ReCycle/gps/.info
 	fi
 
 	#Wait for unmount.
-	while :
-	do
-		echo "Checking for unmount"
-		sleep 3
+	while true; do
+#		echo "Checking for unmount"
+		sleep 5
 		DOUTPUT=`dmesg | tail -f | grep "disconnect" | awk '{print $2}' | cut -d "]" -f 1`
-		if [ -z "$DOUTPUT" ]
-		then
-			echo "USB still inserted, sleeping"
-		else
-			echo "USB disconnected"
+		if [ -n "$DOUTPUT" ]; then
+#			echo "USB disconnected"
 			break
 		fi
 	done #Done with blocking wait for unmount.
